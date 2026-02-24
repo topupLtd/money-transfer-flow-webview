20d2edcf36290723fcddcc9bec153faf7138162b
Mainuddin Ahmed <mainu@Mainuddins-MacBook-Pro.local>
2026-02-24 10:48:44 +0600

    Get auth token from the url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Extract access token from URL query params and store in localStorage
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get("token");
+if (token) {
+  localStorage.setItem("auth_token", token);
+  // Clean the token from the URL to avoid leaking it in bookmarks/history
+  const url = new URL(window.location.href);
+  url.searchParams.delete("token");
+  window.history.replaceState({}, "", url.toString());
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
